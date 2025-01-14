cd26dc3a9e9cbfa106b848558d186e88a7893c4d
蔡志远 <caizhiyuan826@163.com>
2025-01-14 16:44:12 +0800

    conda环境

--- tests/csrt/tracker_test.py
+++ tests/csrt/tracker_test.py
@@ -11,14 +11,14 @@ from app.model import yolo_most_like_box
 from app.track.tracker import InitStrategy, ALG
 
 if __name__ == '__main__':
-    stream = track.tracker.VideoStream(r"D:\video.mp4", True, 1, 1, ALG.MOSSE)
+    stream = track.tracker.VideoStream(r"D:\video.mp4", True, 0, 1, ALG.MOSSE)
     total_frame = int(stream.cap.get(cv2.CAP_PROP_FRAME_COUNT))
     fps = stream.cap.get(cv2.CAP_PROP_FPS)
     duration = total_frame / fps
     start = time.time()
     yolo_ = yolo.yolo4.Yolo4()
     cv2.destroyAllWindows()
-    stream.re_init_strategy(strategy=InitStrategy.BY_UPDATE, min_interval=3 * 1000, max_interval=10 * 1000, interval=30)
+    stream.re_init_strategy(strategy=InitStrategy.BY_UPDATE, min_interval=3 * 1000, max_interval=10 * 1000, interval=100)
     stream.track(lambda frm: yolo_most_like_box(yolo_, frm, confidence=0.1, class_id=15)[1])
     print(f'track cost:{time.time() - start}, total frame:{total_frame}, fps:{fps}, video duration:{duration}')
     while True:
